b54fb978c8738d4a820cf46de10fdd859bdf25ff
juninjune <r0otstat1c@naver.com>
2020-08-02 22:38:48 +0900

    init

---
+++ Assets/Scripts/SceneLoader/ButtonObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ButtonObject : MonoBehaviour
+{
+    public Animator animator;
+
+    public void LoadGame()
+    {
+        SceneLoader.Instance.LoadNewScene("FactoryBuild");
+    }
+
+    public void LoadTheatre()
+    {
+        SceneLoader.Instance.LoadNewScene("Menu");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        LoadGame();
+        animator.SetTrigger("Click");
+    }
+}

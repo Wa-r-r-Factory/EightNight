da2fdbc93d5545eedbc09c8aff79ddc10a97831d
juninjune <r0otstat1c@naver.com>
2020-08-06 03:27:34 +0900

    공장 모델 업데이트 + 공장 물류 시스템 추가

---
+++ Assets/Scripts/TransportaionSystem/SimpleConveyorbelt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleConveyorbelt : MonoBehaviour
+{
+    public float speed = 0.5f; // 속도
+    public Vector3 direction; // 작동 방향
+
+    private Rigidbody rigid;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 pos = rigid.position;
+        rigid.position += direction * -speed * Time.fixedDeltaTime;
+        rigid.MovePosition(pos);
+    }
+
+    public void TurnDirection()
+    {
+        direction = -1 * direction;
+    }
+}

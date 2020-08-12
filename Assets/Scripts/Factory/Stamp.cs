9d4da914483781011f8ea321fcf68be0e5fc9898
juninjune <r0otstat1c@naver.com>
2020-08-12 13:04:45 +0900

    Workbench update

---
+++ Assets/Scripts/Factory/Stamp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stamp : MonoBehaviour
+{
+    private Animator m_animator;
+
+    private bool isActive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    public void StartStamp()
+    {
+        if (!isActive) StartCoroutine(Stamping());
+    }
+
+    IEnumerator Stamping()
+    {
+        isActive = true;
+        m_animator.SetTrigger("Stamp");
+        yield return new WaitForSeconds(1f);
+
+        isActive = false;
+        yield return null;
+
+    }
+}
---
+++ Assets/Scripts/Factory/TransportaionSystem/ProductGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CreateProduct());
+    }
+
+    IEnumerator CreateProduct()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            GameObject product = ProductPoolingManager.instance.GetQueue();
+            product.transform.position = transform.position;
+            product.transform.rotation = transform.rotation;
+        }
+    }
+}

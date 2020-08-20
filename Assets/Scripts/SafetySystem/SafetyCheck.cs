4a562c05b92fcf3c2debc67f3d343ad9d4f3ede5
juninjune <r0otstat1c@naver.com>
2020-08-20 17:07:34 +0900

    안전 시스템 보완

--- Assets/Scripts/SafetySystem/SafetyCheck.cs
+++ Assets/Scripts/SafetySystem/SafetyCheck.cs
@@ -11,6 +11,7 @@ public class SafetyCheck : MonoBehaviour
     private float safetyRange = 1f;
     // 나눗셈 연산을 한번만 실행시키기 위한 변수
     private float reciprocalSafetyRange;
+    private bool isActive = false;
 
     private void Start()
     {
@@ -20,6 +21,7 @@ public class SafetyCheck : MonoBehaviour
         for (int i = 0; i < Warnings.Length; i++)
         {
             meshes.Add(Warnings[i].GetComponent<MeshRenderer>());
+            meshes[i].enabled = isActive;
         }
 
     }
@@ -38,8 +40,6 @@ public class SafetyCheck : MonoBehaviour
         {
                 float dis = hit.distance;
                 dangerDegree = (safetyRange - dis) * reciprocalSafetyRange;
-
-                SetAlpha(meshes, dangerDegree);
         }
 
         if (Physics.Raycast(transform.position, transform.forward + transform.right, out hit, safetyRange, layer))
@@ -47,7 +47,7 @@ public class SafetyCheck : MonoBehaviour
             float dis = hit.distance;
             float newDangerDegree = (safetyRange - dis) * reciprocalSafetyRange;
 
-            if(newDangerDegree > dangerDegree) SetAlpha(meshes, newDangerDegree);
+            if (newDangerDegree > dangerDegree) dangerDegree = newDangerDegree;
         }
 
         if (Physics.Raycast(transform.position, transform.forward - transform.right, out hit, safetyRange, layer))
@@ -55,9 +55,28 @@ public class SafetyCheck : MonoBehaviour
             float dis = hit.distance;
             float newDangerDegree = (safetyRange - dis) * reciprocalSafetyRange;
 
-            if (newDangerDegree > dangerDegree) SetAlpha(meshes, newDangerDegree);
+            if (newDangerDegree > dangerDegree) dangerDegree = newDangerDegree;
         }
 
+        if(dangerDegree > 0.05f && !isActive)
+        {
+            for (int i = 0; i < meshes.Count; i++)
+            {
+                meshes[i].enabled = true;
+            }
+            isActive = true;
+        }
+
+        if (dangerDegree < 0.05f && isActive)
+        {
+            for (int i = 0; i < meshes.Count; i++)
+            {
+                meshes[i].enabled = false;
+            }
+            isActive = false;
+        }
+
+        if(isActive) SetAlpha(meshes, dangerDegree);
     }
 
     void SetAlpha(List<MeshRenderer> meshes, float alphaValue)

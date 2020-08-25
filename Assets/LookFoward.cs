2a129529200b6e287510696b949855c37fe21a0b
juninjune <r0otstat1c@naver.com>
2020-08-25 17:03:26 +0900

    라디오

---
+++ Assets/LookFoward.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookFoward : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject cam = GameObject.FindGameObjectWithTag("MainCamera");
+
+        transform.parent = cam.transform;
+
+        transform.localPosition = new Vector3(0f, 0f, 0.5f);
+        transform.LookAt(cam.transform);
+    }
+
+}

ef971131aa3587ed2215e52b966b128b6ecb4160
juninjune <r0otstat1c@naver.com>
2020-08-25 18:09:33 +0900

    House Scene 정리

---
+++ Assets/Scripts/House/Notepad.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notepad : MonoBehaviour
+{
+    private bool isActive = false;
+    private Animator m_animator;
+
+    public GameObject dialog;
+
+    private void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    public void Activate()
+    {
+        Debug.Log("불렀어?");
+
+        if (!isActive)
+        {
+            isActive = true;
+
+            m_animator.SetTrigger("Open");
+            dialog.SetActive(true);
+        }
+        else
+        {
+            isActive = false;
+
+            m_animator.SetTrigger("Close");
+            dialog.SetActive(false);
+        }
+    }
+
+}
---
+++ Assets/Scripts/InitDialog.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitDialog : MonoBehaviour
+{
+    public Transform parent;
+
+    private void Start()
+    {
+        transform.parent = parent;
+    }
+}

158bab4f9de261d7a35a1f67aa673708d0d97ed5
juninjune <r0otstat1c@naver.com>
2020-08-26 21:51:00 +0900

    사운드 위치 업데이트

--- Assets/Scripts/House/Notepad.cs
+++ Assets/Scripts/House/Notepad.cs
@@ -6,12 +6,17 @@ public class Notepad : MonoBehaviour
 {
     private bool isActive = false;
     private Animator m_animator;
+    private AudioSource audio;
 
     public GameObject dialog;
+
+    public AudioClip openSound;
+    public AudioClip closeSound;
 
     private void Start()
     {
         m_animator = GetComponent<Animator>();
+        audio = GetComponentInParent<AudioSource>();
     }
 
     public void Activate()
@@ -23,6 +28,7 @@ public class Notepad : MonoBehaviour
             isActive = true;
 
             m_animator.SetTrigger("Open");
+            audio.PlayOneShot(openSound);
             dialog.SetActive(true);
         }
         else
@@ -30,6 +36,7 @@ public class Notepad : MonoBehaviour
             isActive = false;
 
             m_animator.SetTrigger("Close");
+            audio.PlayOneShot(closeSound);
             dialog.SetActive(false);
         }
     }

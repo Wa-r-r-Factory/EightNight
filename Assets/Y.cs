c0d26c2c0581afa9ca4d8ab1bbfa0f50c169d6de
juninjune <r0otstat1c@naver.com>
2020-08-26 14:29:03 +0900

    Y 추가

---
+++ Assets/Y.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Y : MonoBehaviour
+{
+    public GameObject[] dialogs;
+
+    private bool isPlaying = false;
+    private SceneCaller sceneCaller;
+
+    private void Start()
+    {
+        sceneCaller = GetComponent<SceneCaller>();
+    }
+
+    public void PlayScript() 
+    {
+        if(!isPlaying) StartCoroutine(ScriptPlay());
+    }
+
+
+    IEnumerator ScriptPlay()
+    {
+        isPlaying = true;
+
+        for (int i = 0; i < dialogs.Length; i++)
+        {
+            dialogs[i].SetActive(true);
+
+            yield return null;
+            yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+
+            dialogs[i].SetActive(false);
+        }
+
+        yield return new WaitForSeconds(2f);
+
+        isPlaying = false;
+        sceneCaller.LoadTelephone();
+    }
+}
